9440a2b9efa252528a29a39be5cf3fcbf43f856f
gear_kinu <techgit1024@gmail.com>
2025-06-21 16:17:53 +0900

    update: copyright

--- src/components/copyright.tsx
+++ src/components/copyright.tsx
@@ -3,7 +3,7 @@ import { Text, type TextProps } from '@chakra-ui/react'
 export const Copyright = (props: TextProps) => {
   return (
     <Text fontSize="sm" color="fg.muted" {...props}>
-      &copy; {new Date().getFullYear()} Logo, Inc. All rights reserved.
+      &copy; {new Date().getFullYear()}
     </Text>
   )
 }
